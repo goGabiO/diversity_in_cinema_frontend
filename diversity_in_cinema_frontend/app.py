9169330ea6c144ba82a8bb41512e19142e4d5d12
Gabriella Otero <gabriella.otero@gmail.com>
2021-11-25 15:04:59 +0100

    Updated Streamlit code to pull CSV according to search terms

--- diversity_in_cinema_frontend/app.py
+++ diversity_in_cinema_frontend/app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import base64
 import pandas as pd
+import matplotlib.pyplot as plt
 
 CSS = """
 h1 {
@@ -12,9 +13,11 @@ h2 {
 h3 {
     color: white;
 }
+p {
+    color: white;
+}
 """
 st.write(f'<style>{CSS}</style>', unsafe_allow_html=True)
-
 
 @st.cache
 def load_image(path):
@@ -26,7 +29,7 @@ def load_image(path):
 
 def image_tag(path):
     encoded = load_image(path)
-    tag = f'<img src="data:il_fullxfull.31046271/webp;base64,{encoded}">'
+    tag = f'<img src="data:faces/webp;base64,{encoded}">'
     return tag
 
 
@@ -35,7 +38,7 @@ def background_image_style(path):
     style = f'''
     <style>
     .stApp {{
-        background-image: url("data:il_fullxfull.31046271/webp;base64,{encoded}");
+        background-image: url("data:faces/webp;base64,{encoded}");
         background-size: cover;
     }}
     </style>
@@ -43,24 +46,35 @@ def background_image_style(path):
     return style
 
 
-image_path = 'il_fullxfull.31046271.webp'
+image_path = 'faces.webp'
 image_link = '~/code/moe221/diversity_in_cinema_frontend/'
 
 st.write(background_image_style(image_path), unsafe_allow_html=True)
+
 '''
 # Diversity and Representation in Hollywood
 '''
 from PIL import Image
 
 image = Image.open('movie_reel.png')
-st.image(image, use_column_width=True)
+st.image(image, use_column_width=True, output_format="PNG")
 '''
 ## Select a movie to view
 '''
-title = st.text_input('Movie title')
+option = st.selectbox('Which movie would you like to view?',
+                      ('Man of Steel', 'Ace Ventura'))
 
-st.write('The current movie title is', title)
+st.write('You selected:', option)
 
-if title != "":
-    df = pd.read_csv('raw_data/man_of_steel_results.csv')
-    st.write(df['frame_number'][0])
+with st.container():
+    if option != "":
+        search_terms = option.lower().split()
+        movie_name = '_'.join(search_terms)
+        file_name = f'raw_data/{movie_name}_results.csv'
+        df = pd.read_csv(file_name)
+        x = df["gender"].value_counts().index
+        y = df["gender"].value_counts().values
+        st.write('Statistics from:', movie_name)
+        fig, ax = plt.subplots()
+        plt.bar(x, y)
+        st.pyplot(fig)
