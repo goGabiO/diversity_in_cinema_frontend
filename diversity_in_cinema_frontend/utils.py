9eed059e8812fe68768fae06f650428392100c0d
Mohamed Abuhalala <myf.abuhalala@gmail.com>
2021-12-01 20:23:35 +0100

    added new visualization functions

--- diversity_in_cinema_frontend/utils.py
+++ diversity_in_cinema_frontend/utils.py
@@ -1,17 +1,17 @@
 from google.cloud import storage
+import re
+import pandas as pd
+from tqdm import tqdm
 from diversity_in_cinema_frontend.params import *
 
-def get_movie_list(subfolders):
 
+def get_movie_list(subfolders):
     """
     Function ro grab file names from a GCP bucket directory
-
     Parameters:
-
     bucket_name: Name of GCP bucket
     subfolders: complete subfolder path as a string where file names should
                 be retrieved from in the format folder_1/folder_2/.../folder_n
-
     """
 
     # check if movie was already processed:
@@ -28,6 +28,40 @@ def get_movie_list(subfolders):
     return file_names
 
 
-if __name__ == "__main__":
+def get_evolution_data():
 
-    print(get_movie_list("CSVs"))
+    df_stats_list = []
+
+    regex = re.compile(r'\((\d{4})\)')
+
+    movie_list = get_movie_list("CSVs")
+
+    for movie in tqdm(movie_list):
+
+        movie = movie.replace(" ","_")
+
+        if movie == "":
+            continue
+
+        print(movie)
+        year = regex.findall(movie)[0]
+
+        df = pd.read_csv(
+            f"gs://{BUCKET_NAME_STREAMLIT}/CSVs/{movie}/statistics",
+            index_col=None)
+
+        df["title"] = movie
+        df["year"] = year
+
+        df["year"] = pd.to_datetime(df["year"].values)
+        df.sort_values("year", inplace=True)
+        df_stats_list.append(df)
+
+    df_stats_total = pd.concat(df_stats_list, axis=0)
+    df_stats_total.sort_values("year", inplace=True)
+
+    return df_stats_total
+
+if __name__ == "__main__":
+    # print(get_movie_list("CSVs"))
+    get_evolution_data()
--- diversity_in_cinema_frontend/visualizations.py
+++ diversity_in_cinema_frontend/visualizations.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import plotly.graph_objects as go
+import plotly.express as px
+
 
 def g_screentime_donut(df):
     x = [df['man_screentime'].values[0], df['woman_screentime'].values[0]]
@@ -65,3 +67,143 @@ def woc_screentime_donut(df):
     go_fig.add_trace(do_fig)
     go_fig.add_annotation(text="Screentime when women of color are present")
     return go_fig
+
+######### Moe's Plots ###########
+
+
+def plot_gender_timeline(df, plot_type="bar", step=5):
+
+    # group by decade
+    df_grouped = df.groupby(
+        pd.cut(df["year"],
+               pd.date_range('1920', '2030', freq=f'{step}YS'),
+               right=False)).mean()
+
+    new_df = pd.DataFrame({
+        'year':
+        pd.date_range(start='01-01-1920', end='01-01-2030', freq=f'{step}YS')
+    })
+    df_stats_total = new_df.merge(df, on='year', how='left')
+    df_grouped = df_stats_total.groupby(
+        pd.Grouper(key='year', freq=f'{step}YS')).mean()
+    df_grouped = df_grouped.dropna()
+
+    if plot_type == "bar":
+        # plot gender over time
+        fig = px.bar(
+            df_grouped,
+            x=df_grouped.index,
+            y=['man_screentime', 'woman_screentime', 'only_men', 'only_women'],
+            barmode="overlay",
+            labels={
+                "value": "Screentime [%]",
+                "year": ""
+            })
+        fig.show()
+
+    elif plot_type == "line":
+        fig = px.line(
+            df_grouped,
+            x=df_grouped.index,
+            y=['man_screentime', 'woman_screentime', 'only_men', 'only_women'],
+            labels={
+                "value": "Screentime [%]",
+                "year": ""
+            })
+
+    return fig
+
+
+
+def plot_race_timeline(df, plot_type="bar", step=5):
+
+    # group by decade
+    df_grouped = df.groupby(
+        pd.cut(df["year"],
+               pd.date_range('1920', '2030', freq=f'{step}YS'),
+               right=False)).mean()
+
+    new_df = pd.DataFrame({
+        'year':
+        pd.date_range(start='01-01-1920', end='01-01-2030', freq=f'{step}YS')
+    })
+    df_stats_total = new_df.merge(df, on='year', how='left')
+    df_grouped = df_stats_total.groupby(
+        pd.Grouper(key='year', freq=f'{step}YS')).mean()
+    df_grouped = df_grouped.dropna()
+
+    if plot_type == "bar":
+        # plot gender over time
+        fig = px.bar(df_grouped,
+                     x=df_grouped.index,
+                     y=[
+                         'asian_screentime', 'black_screentime',
+                         'indian_screentime', 'latino_hispanic_screentime',
+                         'middle_eastern_screentime', 'white_screentime',
+                         'women_of_color'
+                     ],
+                     barmode="overlay",
+                     labels={
+                         "value": "Screentime [%]",
+                         "year": ""
+                     })
+        fig.show()
+
+    elif plot_type == "line":
+        fig = px.line(df_grouped,
+                      x=df_grouped.index,
+                      y=[
+                          'asian_screentime', 'black_screentime',
+                          'indian_screentime', 'latino_hispanic_screentime',
+                          'middle_eastern_screentime', 'white_screentime',
+                          'women_of_color'
+                      ],
+                      labels={
+                          "value": "Screentime [%]",
+                          "year": ""
+                      })
+    return fig
+
+
+def race_screentime_bar(df):
+    """
+    Input: Original movie overview dataframe
+    """
+
+    one_movie_race = df[[
+        'asian_screentime', 'black_screentime', 'indian_screentime',
+        'latino_hispanic_screentime', 'middle_eastern_screentime',
+        'white_screentime'
+    ]]
+
+    one_movie_race = one_movie_race.T
+
+    fig = px.bar(one_movie_race,
+                 x=one_movie_race.index,
+                 y=one_movie_race[0],
+                 labels={
+                     "index": "",
+                     "0": "Screentime [%]"
+                 },
+                 color=one_movie_race.index)
+    return fig
+
+
+def man_woman_screentime_bar(df):
+    """
+    Input: Original movie overview dataframe
+    """
+
+    one_movie_gender = df[["man_screentime", "woman_screentime"]]
+    one_movie_gender = one_movie_gender.T
+
+    fig = px.bar(one_movie_gender,
+                 x=one_movie_gender.index,
+                 y=one_movie_gender[0],
+                 labels={
+                     "index": "",
+                     "0": "Screentime [%]"
+                 },
+                 color=one_movie_gender.index)
+
+    return fig
